b2a111256161ec5441655c1f3df154f7dfea7a98
tim707 <60167722+tim707@users.noreply.github.com>
2020-02-17 17:43:29 -0500

    Update CurrencyAPI.py

--- API/CurrencyAPI.py
+++ API/CurrencyAPI.py
@@ -1,79 +1,99 @@
+
 #!/usr/bin/env python
 import requests, json, pika, datetime
 
-'''
-credentials = pika.PlainCredentials('test', 'test')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.248',5672,'/',credentials))
-channel.basic_publish(exchange='', routing_key='APItoDatabase', body=jsonNew)
+#initiates rabbitMQ connection
+credentials = pika.PlainCredentials('tim', 'test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.76',5672,'/',credentials))
+channel=connection.channel()
 
-'''
+#gets current currency values from api
+def currencyGet(fromCurrency, toCurrency, amount):
+    url = "https://currency-converter5.p.rapidapi.com/currency/convert"
 
-'''
+    querystring = {"format": "json", "to": toCurrency, "from": fromCurrency, "amount": amount}
+
+    headers = {
+        'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
+        'x-rapidapi-key': "337a8f51c0msh6b1ec7345117496p180156jsne0c864b922e0"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response
+
+#gets historical currency values from api
+def historicalGet(fromCurrency, toCurrency, amount, time):
+    url = "https://currency-converter5.p.rapidapi.com/currency/historical/" + time
+
+    querystring = {"format": "json", "to": toCurrency, "from": fromCurrency, "amount": amount}
+
+    headers = {
+        'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
+        'x-rapidapi-key': "337a8f51c0msh6b1ec7345117496p180156jsne0c864b922e0"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response
+#Function that is acitivted when api data is requested
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    currencyGet()
 
-channel.basic_consume(queue='APItoDatabase',
-                      auto_ack=True,
-                      on_message_callback=callback)
-
-
-#Function that requests data from the api returns json response
-
-'''
-amount='10'
-fromCurrency='JPY'
-#gets current currency values
-def currencyGet(fromCurrency,toCurrency, amount):
-    url="https://currency-converter5.p.rapidapi.com/currency/convert"
-
-    querystring = {"format":"json","to":toCurrency,"from":fromCurrency,"amount":amount}
-
-    headers = {
-    'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
-    'x-rapidapi-key': "337a8f51c0msh6b1ec7345117496p180156jsne0c864b922e0"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response
-    
-
-#gets historical currency values
-def historicalGet(fromCurrency,toCurrency, amount,time ):
-    url = "https://currency-converter5.p.rapidapi.com/currency/historical/"+time
- 
-    querystring = {"format":"json","to":toCurrency,"from":fromCurrency,"amount":amount}
-
-    headers = {
-    'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
-    'x-rapidapi-key': "337a8f51c0msh6b1ec7345117496p180156jsne0c864b922e0"
-    }
-    
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response
-
-#creates python readable data
-currentResponse=json.loads((currencyGet('USD', fromCurrency,amount).text))
-
-#converts returned datatime to datetime object
-time=datetime.datetime.strptime(currentResponse['updated_date'],"%Y-%M-%d")
-
-#length of time to be compared to
-timePeriod=2592000
-
-#converts datetime object to POISX and subtracts from the end date and finds difference
-monthAgo=datetime.datetime(time.year,time.month,time.day,time.hour,time.second).timestamp()-timePeriod
+    #creates python readable message data
+    body=json.loads(body)
+	fromCurrency=body['currency']
+    amount=body['amount']
+    toCurrency='USD'
+	
+	#gets current currency values
+    currentResponse=json.loads((currencyGet(fromCurrency,toCurrency,amount).text))
+	
+	#converts returned datatime to datetime object
+    time=datetime.datetime.strptime(currentResponse['updated_date'],"%Y-%M-%d")
+	
+	#length of time in seconds to look at in the past
+    timePeriod=2592000
+    monthAgo=datetime.datetime(time.year,time.month,time.day,time.hour,time.second).timestamp()-timePeriod
+    monthAgo=datetime.date.fromtimestamp(monthAgo)
+	
+	#gets historical currency values
+    historicalResponse=json.loads(historicalGet(fromCurrency,toCurrency,amount,str(monthAgo.year)+'-'+str(monthAgo.month)+'-'+str(monthAgo.day)).text)
+	#Finds difference between old and new currency values
+    difference=float(currentResponse['rates'][fromCurrency]['rate'])-float(historicalResponse['rates'][fromCurrency]['rate'])
+	#finds percent difference
+    percent=difference/float(currentResponse['rates'][fromCurrency]['rate'])
+    response={"currency":fromCurrency,"difference":percent*10000}
+	
+	#adds % difference to queue for database
+    channel.basic_publish(exchange='', routing_key='ApiToDatab', body=json.dumps(response))
 
 
-#converts result of subtraction into datetime object
-monthAgo=datetime.date.fromtimestamp(monthAgo)
+#defines what should be done when queue has a new message (start callback function)
+channel.basic_consume(queue='DataToApi',
+                     auto_ack=True,
+                     on_message_callback=callback)
+
+#starts loop checking the queue for any new messages
+channel.start_consuming()
 
 
-#creates python readable data
-historicalResponse=json.loads(historicalGet('USD',fromCurrency,'10',str(monthAgo.year)+'-'+str(monthAgo.month)+'-'+str(monthAgo.day)).text)
 
-#prints difference
-difference=float(currentResponse['rates'][fromCurrency]['rate'])-float(historicalResponse['rates'][fromCurrency]['rate'])
-print(str(difference))
-print(str(difference/float(currentResponse['rates'][fromCurrency]['rate'])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
