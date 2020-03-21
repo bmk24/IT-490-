0a2485288d24d7eeddedbe716eb754eacc09b89d
tim707 <60167722+tim707@users.noreply.github.com>
2020-03-20 23:57:22 -0400

    Update CurrencyAPI.py
    extended features to include more of the scope of the project.

--- API/CurrencyAPI.py
+++ API/CurrencyAPI.py
@@ -5,10 +5,9 @@ times=datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')
 def error(code,message,time):
  
  try:
-  connect=mysql.connector.connect(user='myuser',password='Marioplayer1*',host='192.168.0.15',database='elsdb')
+  connect=mysql.connector.connect(user='myuser',password='PASSWORD',host='3.21.114.39',database='marioGalaxy')
  except:
-  print('Database VM is down.')
-  sys.exit()
+  exit()
  query1="Select * FROM logging"
  
  query="INSERT INTO logging VALUES (%s, %s, %s, %s)"
@@ -17,28 +16,22 @@ def error(code,message,time):
  records=cursor.fetchall()
  cursor.execute(query, (code,"Ubuntu-API", message, time))
  connect.commit()
- for row in records:
-  print(row[0])
-  print(row[1])
-  print(row[2])
-  print(row[3])
  cursor.close()
  connect.close()
 #attempts to connect to rabbitmqvm
 try:
 #initiates rabbitMQ connection
  credentials = pika.PlainCredentials('test', 'test')
- connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.12',5672,'/',credentials))
+ connection = pika.BlockingConnection(pika.ConnectionParameters('ec2-3-21-125-32.us-east-2.compute.amazonaws.com',5672,'/',credentials))
  channel=connection.channel()
 except:
- print("can't connect to rbbitmq vm")
+ 
  error("504","API VM can't connect to rabbitmq VM.",times)
  sys.exit()
 #gets current currency values from api
 
 def currencyGet(fromCurrency, toCurrency, amount):
-
-    url = "https://currency-converter5.p.rapidapi.com/currency/bconvert"
+    url = "https://currency-converter5.p.rapidapi.com/currency/convert"
 
     querystring = {"format": "json", "to": toCurrency, "from": fromCurrency, "amount": amount}
 
@@ -46,12 +39,19 @@ def currencyGet(fromCurrency, toCurrency, amount):
         'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
         'x-rapidapi-key': "337a8f51c0msh6b1ec7345117496p180156jsne0c864b922e0"
     }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    if response.status_code==404:
+   
+    try:
+     response = requests.request("GET", url, headers=headers, params=querystring, timeout=3)
+    except:
+     errors=True
+     times=datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')
+     error('404',"Can't connect to API server from API script.",times)
+     return "error"
+    if error or response.status_code==404:
+       
        times=datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')
        error('404',"Can't connect to API server from API script.",times)
-       sys.exit()
+       return "error"
     return response
 #gets historical currency values from api
 def historicalGet(fromCurrency, toCurrency, amount, time):
@@ -67,21 +67,27 @@ def historicalGet(fromCurrency, toCurrency, amount, time):
     response = requests.request("GET", url, headers=headers, params=querystring)
     if response.status_code==404:
      times=datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')
-     print(error('404',"Can't connect to API server from API script.",times))
-     sys.exit()
+     error('404',"Can't connect to API server from API script.",times)
+     return "error"
     return response
 #Function that is acitivted when api data is requested
 def callback(ch, method, properties, body):
-
-    #creates python readable message data
-    body=json.loads(body)
-    fromCurrency=body['currency']
-    amount=body['amount']
-    toCurrency='USD'
-	
-    #gets current currency values
-    currentResponse=json.loads((currencyGet(fromCurrency,toCurrency,amount).text))
     
+    #creates python readable message data
+    
+    body=json.loads(body)
+    
+    fromCurrency=body['currency']
+    amount=1
+    toCurrency='USD'
+    #gets current currency values
+    
+    toload=currencyGet(fromCurrency,toCurrency,amount)
+    if toload=="error":
+     return
+    currentResponse=json.loads(toload.text)
+
+
     #converts returned datatime to datetime object
     time=datetime.datetime.strptime(currentResponse['updated_date'],"%Y-%m-%d")
     
@@ -90,7 +96,12 @@ def callback(ch, method, properties, body):
     monthAgo=datetime.datetime(time.year,time.month,time.day,time.hour,time.second).timestamp()-timePeriod
     monthAgo=datetime.date.fromtimestamp(monthAgo)
     #gets historical currency values
+    if historicalGet(fromCurrency,toCurrency,amount,str(monthAgo.year)+'-'+str(monthAgo.month)+'-'+str(monthAgo.day))=="error":
+     return
+
     historicalResponse=json.loads(historicalGet(fromCurrency,toCurrency,amount,str(monthAgo.year)+'-'+str(monthAgo.month)+'-'+str(monthAgo.day)).text)
+
+    
     #Finds difference between old and new currency values
     difference=float(currentResponse['rates'][toCurrency]['rate'])-float(historicalResponse['rates'][toCurrency]['rate'])
     #finds percent difference
@@ -98,7 +109,7 @@ def callback(ch, method, properties, body):
     response={"currency":fromCurrency,"difference":percent*1000}
     #adds % difference to queue for database after rounding the value to the nearest
     #divisible by 5 number
-    print(response['difference'])
+    
     roundedDifference=int(round(response['difference']))
     start=roundedDifference-roundedDifference % 5
     stop=start+5
@@ -106,6 +117,14 @@ def callback(ch, method, properties, body):
       response["difference"]=start
     else:
       response["difference"]=stop
+    #upper and lower bound variables
+    maxval=75
+    minval=-75
+    #sets upper and lower limit on currency value
+    if response["difference"]>maxval:
+       response["difference"]=maxval
+    elif response["difference"]<minval:
+       response["difference"]=minval
     channel.basic_publish(exchange='', routing_key='ApiToDatab', body=json.dumps(response))
 
 
@@ -114,7 +133,6 @@ channel.queue_declare(queue='DataToApi', durable=True)
 channel.basic_consume(queue='DataToApi',
                      auto_ack=True,
                      on_message_callback=callback)
-
 
 channel.queue_declare(queue='ApiToDatab', durable=True)
 #starts loop checking the queue for any new messages
