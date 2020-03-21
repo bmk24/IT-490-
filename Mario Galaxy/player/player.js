d892a779deb26fe2ea6760af0e2382f79b296900
bmk24 <42561162+bmk24@users.noreply.github.com>
2020-03-21 12:08:47 -0400

    New Levels, More Mechanics
    Game Timer
    More Basic Levels
    Moving Mystery Box

---
+++ Mario Galaxy/player/player.js
@@ -0,0 +1,24 @@
+var playerStats = {
+    spriteSize: 16,
+        gravity: {
+           x: 0,
+           y: 0.3
+        }, 
+        defaultVelocity: {
+           x: 1.5,
+           y: 16
+        },
+        /* Movement speed when the key is pressed */
+        playerSpeed: {
+           jump: 6.5,
+           left: 0.2,
+           right: 0.2
+        },
+        player: {
+           x: 5,
+           y: 5,
+           spriteType: 'mario',
+           jumping: false,
+           health : 3
+        },
+        };
