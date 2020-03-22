e76417301b0e00d88fb073c9aed62ed01f90337f
tim707 <60167722+tim707@users.noreply.github.com>
2020-03-21 22:23:30 -0400

    Update receive.py

--- API/receive.py
+++ API/receive.py
@@ -25,8 +25,32 @@ def getCurrency(jsonresult):
 
  mycursor.execute(query,(jsonresult,))
  result=mycursor.fetchone()
- 
  channel.basic_publish(exchange='',routing_key='DataToApp',body=json.dumps({'currency':jsonresult,'difference':Decimal(result[0])},use_decimal=True))
+def getPoints(jsonresult):
+ global mydb
+
+ mycursor = mydb.cursor()
+ query="select currentPoints from userInfo where username=%s"
+
+ mycursor.execute(query,(jsonresult,))
+ result=mycursor.fetchone()
+ channel.basic_publish(exchange='',routing_key='DataToApp',body=json.dumps({'username':jsonresult,'points':Decimal(result[0])},use_decimal=True))
+
+def getCharacter(jsonresult):
+ global mydb
+ mycursor = mydb.cursor()
+ query="select * from characters where name=%s"
+
+ mycursor.execute(query,(jsonresult,))
+ result=mycursor.fetchall()
+ result=result[0]
+ channel.basic_publish(exchange='',routing_key='DataToApp',body=json.dumps({'name':jsonresult,'health':Decimal(result[1]),
+ 'speed':Decimal(result[2]),
+ 'jumpHeight':Decimal(result[3]),
+ 'ability':result[4],
+ 'special':result[5],
+ },use_decimal=True))
+
 def getUser(usernames):
  global mydb
  mycursor = mydb.cursor()
@@ -38,22 +62,53 @@ def getUser(usernames):
   currentlevel=row[2]
   currentpoints=row[3]
   maxpoints=row[4]
-  spritepack=row[5]
-  charactername=row[6]
-  currencycode=row[7]
+  playerLives=row[5]
+  spritepack=row[6]
+  charactername=row[7]
+  currencycode=row[8]
   
  channel.basic_publish(exchange='',routing_key='DataToApp',body=json.dumps({'username':usernames,
- 'currentLevel':currentlevel,
+ 'currentLevel':int(currentlevel),
+ 'levelsComplete':int(levelscomplete),
  'currentPoints':Decimal(currentpoints),
  'maxPoints':Decimal(maxpoints),
+ 'playerLives':int(playerLives),
  'spritePack':spritepack,
  'characterName':charactername,
  'currencyCode':currencycode},use_decimal=True))
+def setCurrency(currency,username):
+ global mydb
+ mycursor = mydb.cursor()
+ query="Update userInfo Set currencyCode=%s where username=%s"
+ mycursor.execute(query,(currency,username))
+ mydb.commit()
+ #channel.basic_publish(exchange='',routing_key='DataToApp',body=json.dumps({'username':usernames
+ 
+def setCharacter(character,username):
+ global mydb
+ mycursor = mydb.cursor()
+ query="Update userInfo Set characterName=%s where username=%s"
+ mycursor.execute(query,(character,username))
+ mydb.commit()
+ #channel.basic_publish(exchange='',routing_key='DataToApp',body=json.dumps({'username':usernames
+ 
+def setPoints(points,username):
+ global mydb
+ mycursor = mydb.cursor()
+ query="Update userInfo Set currentPoints=%s where username=%s"
+ mycursor.execute(query,(points,username))
+ mydb.commit()
+def setInfo(username,levelsComplete,currentLevel,currentPoints,maxPoints,currentLives,spritePack,characterName,currencyCode):
+ global mydb
+ mycursor = mydb.cursor()
+ query="Update userInfo Set levelsComplete=%s, currentLevel=%s, currentPoints=%s, maxPoints=%s, playerLives=%s, spritePack=%s, characterName=%s, currencyCode=%s where username=%s"
+ mycursor.execute(query,(levelsComplete,currentLevel, currentPoints, maxPoints, currentPoints, spritePack, characterName, currencyCode, username))
+ mydb.commit()
+ #channel.basic_publish(exchange='',routing_key='DataToApp',body=json.dumps({'username':usernames
  
 def callback(ch, method, properties, body):
  global connection
  jsonResult=json.loads(body)
-
  head=jsonResult["head"]
 
  if head=="getCurrency":
@@ -62,6 +117,36 @@ def callback(ch, method, properties, body):
  elif head=="getUser":
   username=jsonResult['username']
   getUser(username)
+ elif head=="setCurrency":
+  username=jsonResult['username']
+  currency=jsonResult['currency']
+  setCurrency(currency, username)
+ elif head=="setPoints":
+  points=jsonResult['points']
+  username=jsonResult['username']
+  setPoints(points, username)
+ elif head=="getPoints":
+  points=jsonResult['username']
+  getPoints(points)
+ elif head=="getCharacter":
+  character=jsonResult['character']
+  getCharacter(character)
+ elif head=="setCharacter":
+  character=jsonResult['character']
+  username=jsonResult['username']
+  setCharacter(character, username)
+ elif head=="setInfo":
+  username=jsonResult['username']
+  levelsComplete=jsonResult['levelsComplete']
+  currentLevel=jsonResult['currentLevel']
+  currentPoints=jsonResult['currentPoints']
+  maxPoints=jsonResult['maxPoints']
+  currentLives=jsonResult['currentLives']
+  spritePack=jsonResult['spritePack']
+  character=jsonResult['character']
+  currencyCode=jsonResult['currency']
+  setInfo(username,levelsComplete,currentLevel,currentPoints,maxPoints,currentLives,spritePack,character,currencyCode)
+
 channel.basic_consume(queue='AppToData',
                       auto_ack=True,
                       on_message_callback=callback)
