7406888872f3e1fdbdbe86414d818d21813397e5
tim707 <60167722+tim707@users.noreply.github.com>
2020-03-21 22:35:53 -0400

    Update getData.php
    Updated the functions to reflect the current state of the script. Many additional functions were added.

--- API/getData.php
+++ API/getData.php
@@ -1,5 +1,6 @@
 
 <?php
+set_time_limit (4); 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -23,8 +24,55 @@ else if ($head=="getUser"){
  $pass=array("head"=>"getUser","username"=>$username);
 
 }
+else if ($head=="setCurrency"){
+ $currency=$_GET['currency'];
+ $username=$_GET['username'];
+ $pass=array("head"=>"setCurrency","username"=>$username,"currency"=>$currency);
 
+}
+else if ($head=="setPoints"){
+ $points=$_GET['points'];
+ $username=$_GET['username'];
+ $pass=array("head"=>"setPoints","username"=>$username,"points"=>$points);
 
+}
+else if ($head=="getPoints"){
+ $username=$_GET['username'];
+ $pass=array("head"=>"getPoints","username"=>$username);
+
+}
+else if ($head=="getCharacter"){
+ $character=$_GET['character'];
+ $pass=array("head"=>"getCharacter","character"=>$character);
+
+}
+else if ($head=="setCharacter"){
+ $username=$_GET['username'];
+ $character=$_GET['character'];
+ $pass=array("head"=>"setCharacter","username"=>$username,"character"=>$character);
+
+}
+else if ($head=="setInfo"){
+ $username=$_GET['username'];
+ $levelsComplete=$_GET['levelsComplete'];
+ $currentLevel=$_GET['currentLevel'];
+ $currentPoints=$_GET['currentPoints'];
+ $maxPoints=$_GET['maxPoints'];
+ $spritePack=$_GET['spritePack'];
+ $character=$_GET['character'];
+ $currency=$_GET['currency'];
+ $currentLives=$_GET['currentLives'];
+ $pass=array("head"=>"setInfo","username"=>$username,
+ "levelsComplete"=>$levelsComplete,
+ "currentLevel"=>$currentLevel,
+ "maxPoints"=>$maxPoints,
+ "currentLives"=>$currentLives,
+ "currentPoints"=>$currentPoints,
+ "spritePack"=>$spritePack,
+ "character"=>$character,
+ "currency"=>$currency);
+
+}
 //sends json message
 $pass=json_encode($pass);
 $msg = new AMQPMessage($pass);
