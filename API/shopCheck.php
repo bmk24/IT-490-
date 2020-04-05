b491a1982ba3a8421e4784da5dc8865042a4e33b
tim707 <60167722+tim707@users.noreply.github.com>
2020-04-05 01:46:51 -0400

    Shop check price/user balance made by Tim Chuba
    Checks if an item is able to be purchased and communicates the result back to ajax function purchase in shop.php.

---
+++ API/shopCheck.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$_GET["function"]=1;
+include '/var/www/html/testApi/getData.php';
+
+$username=$_GET["username"];
+$itemID=$_GET["itemID"];
+
+
+$quantity=$_GET["quantity"];
+function updateShop($username,$itemID,$quantity){
+
+$_GET["head"]="getUser";
+$_GET["function"]=1;
+$_GET["username"]=$username;
+$arrayUser=json_decode(getData());
+$balance=$arrayUser->currentPoints;
+
+
+$_GET["head"]="getShop";
+$_GET["itemID"]=$itemID;
+$arrayShop=json_decode(getData());
+
+$price=$arrayShop->itemPrice;
+
+if ($price*$quantity<=$balance){
+return 1;
+}
+else {
+return 0;
+}
+}
+echo updateShop($username,$itemID,$quantity);
+?>
