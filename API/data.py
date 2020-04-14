448d829408f92079802d1364fda82df4d30b55bd
tim707 <60167722+tim707@users.noreply.github.com>
2020-04-14 18:37:58 -0400

    Update data.py

--- API/data.py
+++ API/data.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
-import pika, json
+import pika, json, sys, mysql.connector
 #publishes json to be processed and returned by CurrencyAPI.py
-credentials = pika.PlainCredentials('tim','test')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.68',5672,'/',credentials))
+credentials = pika.PlainCredentials('test','test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('ec2-3-21-125-32.us-east-2.compute.amazonaws.com',5672,'/',credentials))
 channel = connection.channel()
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'JPY','amount':20}))
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'EUR','amount':20}))
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'RUB','amount':20}))
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'GBP','amount':20}))
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'ZAR','amount':20}))
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'MXN','amount':20}))
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'CNY','amount':20}))
-channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'CHF','amount':20}))
-
+queue = channel.queue_declare(
+    queue="ApiToDatab", durable=True,
+    exclusive=False, auto_delete=False
+)
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myuser",
+  passwd="MarioGalaxy1*",
+  database="marioGalaxy"
+) 
+mycursor = mydb.cursor()
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'JPY'}))
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'EUR'}))
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'RUB'}))
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'GBP'}))
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'ZAR'}))
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'MXN'}))
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'CNY'}))
+channel.basic_publish(exchange='',routing_key='DataToApi',body=json.dumps({'currency':'CHF'}))
+count=0
 def callback(ch, method, properties, body):
-	print("I've received %r" % body)
-	jsonResult=json.loads(body)
-	print(jsonResult['currency']+' '+str(jsonResult['difference']))
-
-
-channel.basic_consume(queue='ApiToDatab', 
-		      auto_ack=True, 
-		      on_message_callback=callback)
+        global count, channel, connection, queue, mydb, mycursor
+        count+=1
+        
+        jsonResult=json.loads(body)
+        currency=jsonResult['currency']
+        difference=jsonResult['difference']
+        query="update currency set difference= %s where currencyCode= %s"
+        parameters=(difference,currency)
+        mycursor.execute(query, parameters)
+        mydb.commit()
+        if count==8:
+         connection.close()
+         mydb.close()
+         sys.exit()
+channel.basic_consume(queue='ApiToDatab',
+                      auto_ack=True,
+                      on_message_callback=callback)
 channel.start_consuming()
