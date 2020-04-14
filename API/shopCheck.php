56042fb4aae071ac6f0c93ff27578749e167f9e6
tim707 <60167722+tim707@users.noreply.github.com>
2020-04-14 18:53:29 -0400

    Update shopCheck.php

--- API/shopCheck.php
+++ API/shopCheck.php
@@ -2,6 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
+if (!isset($_SESSION["username"]) or empty($_SESSION["username"])){
+echo 0;
+exit();
+}
 $_GET["function"]=1;
 include '/var/www/html/testApi/getData.php';
 
@@ -21,14 +26,21 @@ $balance=$arrayUser->currentPoints;
 
 $_GET["head"]="getShop";
 $_GET["itemID"]=$itemID;
+
+
+
 $arrayShop=json_decode(getData());
 
 $price=$arrayShop->itemPrice;
 $quantityUser=$arrayShop->quantity;
 
+$singleQuantityItems=array(2);
+if ($quantityUser>=1 and in_array($itemID,$singleQuantityItems)){
+return 0;
+}
 
 $cost=$price*$quantity;
-if ($cost<=$balance){
+if ($cost<=$balance and $cost>0){
 
 $_GET["head"]="setQuantity";
 $_GET["quantity"]=$quantity+$quantityUser;
