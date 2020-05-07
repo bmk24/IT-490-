8a44ea332c1938a14b1dab4f09bb174617788629
tim707 <60167722+tim707@users.noreply.github.com>
2020-05-06 20:59:02 -0400

    api migration script
    migrates changes from dev to api. AWS charges for elastic ip addresses so address variable needs to be updated frequently. Changed log so it accurately reflects the status of the folder.

---
+++ setup-migration/apiMigrate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+address=3.23.111.83
+if [ $1 == "m" ] || [ $1 == "migrate" ] || [ $1 == "write" ] || [ $1 == "w" ]; 
+then
+date=$(date +%Y-%m-%d-%M-%S)
+mkdir -p /home/ubuntu/backup
+mkdir -p /home/ubuntu/API
+FILES=/home/ubuntu/API/*
+if [ "$(ls -A /home/ubuntu/API)" ]; then
+for output in $FILES
+do
+
+fileName="${output##*/}"
+
+mv $output /home/ubuntu/backup/"$date-$fileName"
+
+done
+scp -i /home/ubuntu/api.pem ubuntu@$address:/home/ubuntu/API/* /home/ubuntu/API/ 
+echo Successfully migrated new files and backed up $date files.
+echo Successfully migrated new files and backed up $date files. >> /home/ubuntu/migrateLog
+else
+echo Successfully migrated new files. API empty
+echo Successfully migrated new files. API empty >> /home/ubuntu/migrateLog 
+scp -i /home/ubuntu/api.pem ubuntu@$address:/home/ubuntu/API/* /home/ubuntu/API/ 
+fi
+
+
+
+elif [[ $1 == "r" ]] || [[ $1 == "revert" ]]
+then 
+
+
+for output in "/home/ubuntu/backup"/$2*
+do
+
+fileName="${output##*/}"
+filename2=${fileName:17}
+mv $output /home/ubuntu/API/$filename2
+
+done 
+echo Successfully reverted $date files back to API folder  
+echo Successfully reverted $date files back to API folder >> /home/ubuntu/migrateLog
+else
+ echo please input a valid migrate or revert operation
+ echo please input a valid migrate or revert operation >> /home/ubuntu/migrateLog
+fi
