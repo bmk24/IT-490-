7d2249fdad540c536ef717da19b9a266100baa1f
Ryan Winston <60108036+rw259@users.noreply.github.com>
2020-05-09 23:14:21 -0400

    Updated to include essentials

--- AppServ-Migration/setupAppServ.sh
+++ AppServ-Migration/setupAppServ.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get -y upgrade
 sudo ufw disable
-sudo apt-get install apache2 php composer rsyslogd vim -y
+sudo apt-get install apache2 php composer rsyslogd php-bcmath php-mbstring composer require php-amqplib/php-amqplib -y
