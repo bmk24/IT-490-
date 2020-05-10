05bccf622183a3455d2eb0105b949413b4f3e4ba
Ryan Winston <60108036+rw259@users.noreply.github.com>
2020-05-09 22:37:14 -0400

    Cleaned up a few mistakes and sentence structures

--- AppServ-Migration/appMigrate.sh
+++ AppServ-Migration/appMigrate.sh
@@ -41,8 +41,8 @@ then
   mv $output /home/ubuntu/APP/$filename2
 
   done
-  echo Successfully reverted $timestamp files back to APP folder
-  echo Successfully reverted $timestamp files back to APP folder >> /home/ubuntu/migrateLog
+  echo Successfully reverted files back to APP folder as of $date.
+  echo Successfully reverted files back to APP folder as of $date. >> /home/ubuntu/migrateLog
   else
    echo please input a valid migrate or revert operation
    echo please input a valid migrate or revert operation >> /home/ubuntu/migrateLog
